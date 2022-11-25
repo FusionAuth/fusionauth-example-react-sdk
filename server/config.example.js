5b3e666d3c719487e47492789d9f15b1dd5e0915
Jim DeBlock <jim@woodridgesoftware.com>
2022-11-25 14:17:14 -0700

    Removed unused config options

--- server/config.example.js
+++ server/config.example.js
@@ -3,16 +3,11 @@ module.exports = {
 	// FusionAuth info (copied from the FusionAuth admin panel)
 	clientID: '<CLIENT_ID>',
 	clientSecret: '<CLIENT_SECRET>',
-	applicationID: '<APPLICATION_ID>',
 	fusionAuthBaseUrl: '<BASE URL>',
-
-	// our FusionAuth api key
-	apiKey: '<API_KEY>',
 
 	// Redirect URI: Where the application with redirect after a login/logout
 	redirectURI: '<REDIRECT_URI>',
 
-	// ports
-	clientPort: 3000,
+	// port
 	serverPort: 9000
 };
